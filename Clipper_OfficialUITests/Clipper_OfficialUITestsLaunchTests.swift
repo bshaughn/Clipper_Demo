157dcac1606a288af3cd3a8fa9c6ab3fb95e3fb0
Bart Shaughnessy <bart@marctrent.ai>
2025-12-02 11:49:44 -0800

    Initial Commit

---
+++ Clipper_OfficialUITests/Clipper_OfficialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Clipper_OfficialUITestsLaunchTests.swift
+//  Clipper_OfficialUITests
+//
+//  Created by Bart Shaughnessy on 12/2/25.
+//
+
+import XCTest
+
+final class Clipper_OfficialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
